9fa5e16d881ea140e2f3c1daad5985abb0696bf2
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-03-05 21:53:35 +0300

    feat: add header

--- react-components/src/components/Layout.tsx
+++ react-components/src/components/Layout.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import './index.css';
 
 export class Layout extends React.Component {
   render = () => (
     <>
       <div className="header">
         <div className="header__wrapper">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
+          <h2 className="header__logotype">
+            <Link to="/">In recognition of codes</Link>
+          </h2>
+
+          <div className="header__navigation">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+          </div>
         </div>
-
-        <Outlet />
-
-        <footer className="container">
-          Личность, которая этим занималась: Али Адыгезалли, на курсе реакт, ОРУ ___ 2023
-        </footer>
       </div>
+
+      <Outlet />
+
+      <footer className="container">
+        Личность, которая этим занималась: Али Адыгезалли, на курсе реакт, ОРУ ___ 2023
+      </footer>
     </>
   );
 }
