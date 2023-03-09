fd56b1eba302be3335e8da7d43e61898fc14f573
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-03-09 13:03:03 +0300

    feat: add logic for search which saved in localstorage

--- react-components/src/components/Search/index.tsx
+++ react-components/src/components/Search/index.tsx
@@ -2,19 +2,44 @@ import React from 'react';
 import './index.css';
 import loupe from '../../assets/loupe-svgrepo-com.svg';
 
-export class Search extends React.Component {
-  state = {
-    value: '',
-  };
+type Unique = {
+  [index: string]: string | number;
+};
 
-  // onInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-  // this.setState({ state: event.target.value });
-  // };
+type State = {
+  [index: string]: string | number;
+};
+
+export class Search extends React.Component<Unique, State> {
+  constructor(props: Unique) {
+    super(props);
+    this.state = {
+      value: localStorage.getItem('searchValue') || '',
+    };
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+  }
+
+  handleChangeInput(event: React.ChangeEvent<HTMLInputElement>): void {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+
+  componentWillUnmount(): void {
+    localStorage.setItem('searchValue', `${this.state.value}`);
+  }
 
   render = () => (
     <div className="input">
       <img className="loops" src={loupe} />
-      <input className="search" type="search" id="search" placeholder="Поиск по карточкам" />
+      <input
+        value={this.state.value}
+        onChange={this.handleChangeInput}
+        className="search"
+        type="search"
+        id="search"
+        placeholder="Поиск по карточкам"
+      />
     </div>
   );
 }
